80bdbac8f1c14bfcaffa52c7ab4739f6b2217c5c
Miewliie <miewliie@gmail.com>
2023-01-20 15:14:03 -0800

    update test to tapping on 'enter city label' to hide keyboard

--- TAUUITests/Tests/TAUUITests.swift
+++ TAUUITests/Tests/TAUUITests.swift
@@ -14,7 +14,7 @@ final class TAUUITests: TAUUITestBase {
         app.staticTexts["welcomeMessage"].tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["enterCity"]/*[[".staticTexts[\"Enter Your City \"]",".staticTexts[\"enterCity\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app.textFields["city"].tap()
-        app/*@START_MENU_TOKEN@*/.staticTexts["enterCity"]/*[[".staticTexts[\"Enter Your City \"]",".staticTexts[\"enterCity\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.staticTexts["enterCity"].tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["Enroll"]/*[[".buttons[\"Enroll\"].staticTexts[\"Enroll\"]",".buttons[\"enrollButton\"].staticTexts[\"Enroll\"]",".staticTexts[\"Enroll\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         XCTAssertTrue(app.staticTexts["Please Enter City"].exists)
         
@@ -25,7 +25,7 @@ final class TAUUITests: TAUUITestBase {
         app.staticTexts["welcomeMessage"].tap()
         XCUIApplication().textFields["city"].tap()
         XCUIApplication().textFields["city"].typeText("London")
-        XCUIApplication().textFields["city"].tap()
+        XCUIApplication().staticTexts["enterCity"].tap()
         XCUIApplication().buttons["enrollButton"].tap()
         XCTAssertTrue(XCUIApplication().staticTexts["Thanks for Joining!"].exists)
     }
